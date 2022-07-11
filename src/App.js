8c34bba8ac448e9998f531089a281ae08a5ce9f1
izaiasjr <up1.izaiasjr@gmail.com>
2022-07-11 19:54:06 -0300

    meu desafio de react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import fruit from "./img-desafio/Maçã";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    name: "Izaias Junior",
+    age: 27,
+    favoriteFood: "churrasco",
+    favoriteMusic:["Last Night", "Living on a Prayer", "Apologize"]
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <h2>{this.state.age}</h2>
+        <h3>{this.state.favoriteFood}</h3>
+        <h4>Fruta Favorita</h4>
+        <img src={fruit} title="Maçã" alt="Maçã-img"/>
+        <h2>Música Favorita</h2>
+        <ul>
+          <li>{this.state.favoriteMusic[0]}</li>
+          <li>{this.state.favoriteMusic[1]}</li>
+          <li>{this.state.favoriteMusic[2]}</li>
+        </ul>
+        <img src={maçã} alt="fruta maça"/>
+      </div>
+
+    )
+  }
 }
 
 export default App;
